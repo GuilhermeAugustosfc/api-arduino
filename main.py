bad84f832cb5dec2f5f6ba1ba9e7a5667ecc734c
Guilherme Augusto <guilherme_augustosfc@hotmail.com>
2024-05-20 13:57:30 -0300

    termiando alguns endpoints

---
+++ main.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI, HTTPException, Query, status
+from fastapi.middleware.cors import CORSMiddleware
+from service import (
+    get_disponibilidade,
+    get_eventos_falhas,
+    get_intervalos_falhas,
+    get_produtividade,
+    total_produtos_produzidos,
+    get_historico,
+)
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/disponibilidade/")
+async def disponibilidade(
+    timestamp_inicial: str = Query(...), timestamp_final: str = Query(...)
+):
+    dispo = get_disponibilidade(timestamp_inicial, timestamp_final)
+    return dispo
+
+
+@app.get("/produtividade/")
+async def produtividade(
+    timestamp_inicial: str = Query(...), timestamp_final: str = Query(...)
+):
+    produ = get_produtividade(timestamp_inicial, timestamp_final)
+    return produ
+
+
+@app.get("/total_products_produced/")
+async def total_products_produced(
+    timestamp_inicial: str = Query(...), timestamp_final: str = Query(...)
+):
+    total_produzidos = total_produtos_produzidos(timestamp_inicial, timestamp_final)
+    return total_produzidos
+
+
+@app.get("/machine_failures/")
+async def machine_failures(
+    timestamp_inicial: str = Query(...), timestamp_final: str = Query(...)
+):
+    eventos_falha = get_eventos_falhas(timestamp_inicial, timestamp_final)
+    return eventos_falha
+
+
+@app.get("/intervalos_falha/")
+async def machine_failures(
+    timestamp_inicial: str = Query(...), timestamp_final: str = Query(...)
+):
+    intervalos = get_intervalos_falhas(timestamp_inicial, timestamp_final)
+    return intervalos
+
+
+@app.get("/historico/")
+async def machine_failures(
+    timestamp_inicial: str = Query(...), timestamp_final: str = Query(...)
+):
+    intervalos = get_historico(timestamp_inicial, timestamp_final)
+    return intervalos
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ service.py
@@ -0,0 +1,164 @@
+import mysql.connector
+from datetime import datetime
+
+config = {
+    "host": "database-1.cpigwcyuk6vv.us-east-2.rds.amazonaws.com",
+    "user": "admin",
+    "password": "SO9yvDX5GwQfF1EWEM5u",
+    "database": "mqtt1",
+}
+db_connection = mysql.connector.connect(**config)
+
+cursor = db_connection.cursor()
+
+
+def calcular_diferenca_tempo(dados):
+    total = 0
+    for item in dados:
+        total += item[3].seconds
+
+    return total
+
+
+def get_disponibilidade(timestamp_inicial, timestamp_final):
+
+    # Questão 1: Gráfico de disponibilidade
+    intervalos = get_intervalo_disponibilidade(timestamp_inicial, timestamp_final)
+    tempo_trabalhando = calcular_diferenca_tempo(intervalos)
+
+    cursor.execute(
+        """
+        SELECT total_horas_trabalho
+        FROM maquina
+    """
+    )
+    config_maquina = cursor.fetchone()
+
+    porc_tempo_trabalhado = (tempo_trabalhando / config_maquina[0]) * 100
+    return {
+        "porc_tempo_trabalhado": porc_tempo_trabalhado,
+        "tempo_trabalhando_segundos": tempo_trabalhando,
+        "total_horas_trabalho_segundos": config_maquina[0],
+    }
+
+
+def get_produtividade(timestamp_inicio, timestamp_fim):
+    cursor.execute(
+        """
+        SELECT s.maquina_id, s.producao, m.total_produto,
+            (s.producao / m.total_produto) * 100 AS produtividade
+        FROM sensordata s
+        JOIN maquina m ON s.maquina_id = m.id
+        WHERE s.timestamp BETWEEN %s AND %s
+          AND s.timestamp = (SELECT MAX(timestamp) FROM sensordata WHERE status = 1)
+        """,
+        (timestamp_inicio, timestamp_fim),
+    )
+    produtividade_resultado = cursor.fetchall()
+
+    return produtividade_resultado
+
+
+def total_produtos_produzidos(inicial, final):
+
+    cursor.execute(
+        """
+        SELECT MAX(producao)
+        FROM sensordata
+        WHERE status = 1 AND
+        timestamp BETWEEN %s AND %s
+        ORDER BY timestamp DESC
+        LIMIT 1;
+    """,
+        inicial,
+        final,
+    )
+    dados = cursor.fetchone()
+
+    return dados[0]
+
+
+def get_eventos_falhas(inicial, final):
+
+    cursor.execute(
+        """
+        SELECT *
+        FROM sensordata
+        WHERE status = 0 and motivo is null and timestamp BETWEEN %s AND %s
+    """,
+        inicial,
+        final,
+    )
+    dados = cursor.fetchone()
+
+    return dados
+
+
+def get_intervalo_disponibilidade(timestamp_inicio, timestamp_fim):
+    query = """
+            SELECT 
+            MIN(timestamp) AS inicio_intervalo,
+            MAX(timestamp) AS fim_intervalo,
+            status,
+            sec_to_time(TIMESTAMPDIFF(SECOND, MIN(timestamp), MAX(timestamp))) AS duracao_intervalo
+            FROM (
+                SELECT
+                    timestamp,
+                    status,
+                    @group := IF(@prev_status = status, @group, @group + 1) AS grp,
+                    @prev_status := status
+                FROM
+                    sensordata,
+                    (SELECT @group := 0, @prev_status := NULL) AS vars
+                ORDER BY
+                    timestamp
+            ) AS grouped
+            WHERE 
+                status = 1 and sensordata.timestamp BETWEEN %s AND %s
+            GROUP BY 
+                grp;
+        """
+    cursor.execute(query, (timestamp_inicio, timestamp_fim))
+    dados = cursor.fetchall()
+
+    return dados
+
+
+def get_intervalos_falhas(timestamp_inicio, timestamp_fim):
+
+    query = """
+            SELECT 
+            MIN(timestamp) AS inicio_intervalo,
+            MAX(timestamp) AS fim_intervalo,
+            status,
+            sec_to_time(TIMESTAMPDIFF(SECOND, MIN(timestamp), MAX(timestamp))) AS duracao_intervalo
+            FROM (
+                SELECT
+                    timestamp,
+                    status,
+                    motivo,
+                    @group := IF(@prev_status = status, @group, @group + 1) AS grp,
+                    @prev_status := status
+                FROM
+                    sensordata,
+                    (SELECT @group := 0, @prev_status := NULL) AS vars
+                ORDER BY
+                    timestamp
+            ) AS grouped
+            WHERE 
+                status = 0 and motivo is null and timestamp BETWEEN %s AND %s
+            GROUP BY 
+                grp;
+        """
+    cursor.execute(query, (timestamp_inicio, timestamp_fim))
+    dados = cursor.fetchall()
+    return dados
+
+
+def get_historico(timestamp_inicio, timestamp_fim):
+    query = """
+            SELECT * from sensordata where timestamp BETWEEN %s AND %s
+        """
+    cursor.execute(query, (timestamp_inicio, timestamp_fim))
+    dados = cursor.fetchall()
+    return dados
